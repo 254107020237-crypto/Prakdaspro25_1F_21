fd52b32db8abf14183dd8a63b3cbe2b0ed91bfb0
254107020237-crypto <254107020237@student.polinema.ac.id>
2025-10-09 11:06:44 +0700

    P6_Percobaan3

---
+++ P6/ifElseCetakKRS21.java
@@ -0,0 +1,32 @@
+package P6;
+
+import java.util.Scanner;
+
+public class ifElseCetakKRS21 {
+    public static void main(String[] args) {
+        Scanner Akbar21 = new Scanner(System.in);
+        String pesan;
+        System.out.print("Apakah mahasiswa sudah bebas kompen? (Ya/Tidak): ");
+        String bebasKompen = Akbar21.nextLine().trim();
+
+        System.out.print("Masukkan jumlah log bimbingan Pembimbing 1: ");
+        int bimbinganP1 = Akbar21.nextInt();
+        System.out.print("Masukkan jumlah log bimbingan Pembimbing 2: ");
+        int bimbinganP2 = Akbar21.nextInt();
+
+        if (bebasKompen.equalsIgnoreCase("Ya")) {
+            if (bimbinganP1 >= 8 && bimbinganP2 >= 4) {
+                pesan = "Semua syarat tepenuhi. Mahasiswa boleh mendaftar ujian skripsi";
+            } else if (bimbinganP1 < 8 && bimbinganP2 < 4) {
+                pesan = "Gagal! Log bimbingan P1 kurang dari 8 kali dan P2 kurang dari 4 kali";
+            } else if (bimbinganP1 < 8) {
+                pesan = "Gagal! Log bimbingan P1 belum mencapai 8 kali";
+            } else {
+                pesan = "Gagal! Log bimbingan P2 belum mencapai 4 kali";
+            }
+    
+        } else {
+            pesan = "Gagal! Mahasiswa masih memiliki tanggungan kompen";
+        }
+    }
+}
