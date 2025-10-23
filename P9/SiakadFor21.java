2482ea70700f36a6893ca66d648609d6bfbcc0e6
254107020237-crypto <254107020237@student.polinema.ac.id>
2025-10-23 11:24:09 +0700

    P9_Percobaan1_Pertanyaan4

--- P9/SiakadFor21.java
+++ P9/SiakadFor21.java
@@ -6,6 +6,7 @@ public class SiakadFor21 {
     public static void main(String[] args) {
         Scanner Akbar21 = new Scanner(System.in);
         double nilai, tertinggi=0, terendah=100;
+        int clulus=0, ctdklulus=0;
 
         for (int i =1; i <= 10; i++) {
         }
@@ -19,9 +20,18 @@ public class SiakadFor21 {
             if (nilai < terendah) {
                 terendah = nilai;
             }
+                    if (nilai>=60) {
+            clulus++;
+        }else{
+            ctdklulus++;
         }
+
+        }
+
         System.out.println("Nilai terendah: " +tertinggi);
         System.out.println("Nilai terendah:" +terendah);
+        System.out.println("Jumlah mahasiswa lulus:" +clulus);
+        System.out.println("Jumlah mahasiswa tidak lulus:" +ctdklulus);
     }
 }
 
