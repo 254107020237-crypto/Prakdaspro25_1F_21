9ac1de0bd26dcf05f1780f949bdf8b754e861ba9
254107020237-crypto <254107020237@student.polinema.ac.id>
2025-11-09 19:34:35 +0700

    Tugas1_Pertemuan11

---
+++ P11/P11_Tugas1.java
@@ -0,0 +1,44 @@
+package P11;
+
+import java.util.Scanner;
+
+public class P11_Tugas1 {
+    public static void main(String[] args) {
+        Scanner Akbar21 = new Scanner(System.in);
+
+        int BanyakJumlahMhs, NilaiTertinggi = 0, NilaiTerendah = 0; 
+        double RataRata, total = 0;
+        
+        System.out.print("Masukkan jumlah mahasiswa : ");
+        BanyakJumlahMhs = Akbar21.nextInt();
+        int NilaiMahasiswa [] = new int[BanyakJumlahMhs];
+
+        for (int i = 0; i < BanyakJumlahMhs; i++) {
+            System.out.print("Masukkan Nilai Nilai Mahasiswa ke- "+(i+1)+ " : ");
+            NilaiMahasiswa [i] = Akbar21.nextInt();            
+        } 
+
+        for (int i = 0; i < NilaiMahasiswa.length; i++) {
+            total += NilaiMahasiswa [i];
+        }
+
+        RataRata = total/NilaiMahasiswa.length;
+        System.out.println("Rata - rata nilai = "+RataRata);
+
+         for (int i = 0; i < NilaiMahasiswa.length; i++) {
+            total += NilaiMahasiswa[i];
+            if (NilaiMahasiswa[i] > NilaiTertinggi) {
+                NilaiTertinggi = NilaiMahasiswa[i];
+            } else {
+                if (NilaiMahasiswa[i] > NilaiTerendah) {
+                    NilaiTerendah = NilaiMahasiswa [i];
+                }
+            } 
+        }
+        System.out.println("Nilai Tertinggi = "+NilaiTertinggi);
+        System.out.println("Nilai Terendah = "+NilaiTerendah);
+        }
+    }
+
+
+
