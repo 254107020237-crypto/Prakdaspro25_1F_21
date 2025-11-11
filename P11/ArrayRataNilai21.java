97e4966a94910d4aec712c03d5a1ba18af1a3de0
254107020237-crypto <254107020237@student.polinema.ac.id>
2025-11-11 22:26:21 +0700

    P11_Percobaan3_Pertanyaan1

--- P11/ArrayRataNilai21.java
+++ P11/ArrayRataNilai21.java
@@ -9,6 +9,7 @@ public class ArrayRataNilai21 {
         int [ ] nilaiMhs = new int[10];
         double total = 0;
         double rata2;
+        int mhslulus=0;
 
         for (int i = 0; i < nilaiMhs.length; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-"+(i+1)+" : ");
@@ -17,9 +18,13 @@ public class ArrayRataNilai21 {
 
         for (int i = 0; i < nilaiMhs.length; i++) {
             total += nilaiMhs[i];
+            if (nilaiMhs[i] > 70) {
+            ++mhslulus;
+            }
         }
 
         rata2 = total/nilaiMhs.length;
         System.out.println("Rata - rata nilai = "+rata2);
+        System.out.println("banyak Mahasiswa lulus yaitu : " + mhslulus +  " orang ");
     }
 }
