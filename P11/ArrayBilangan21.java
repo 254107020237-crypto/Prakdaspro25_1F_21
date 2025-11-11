83e66be2be61258df3158076d6b3c3ef0f89e625
254107020237-crypto <254107020237@student.polinema.ac.id>
2025-11-11 21:08:09 +0700

    P11_Percobaan1_Pertanyaan3

--- P11/ArrayBilangan21.java
+++ P11/ArrayBilangan21.java
@@ -6,11 +6,8 @@ public class ArrayBilangan21 {
     public static void main(String[] args) {
         Scanner Akbar21 = new Scanner(System.in);
 
-        int [] bil = {5, 13, -7, 13};
-
-        System.out.println(bil [0]);
-        System.out.println(bil [1]);
-        System.out.println(bil [2]);
-        System.out.println(bil [3]);
+        int [] bill = {5, 13, -7, 13};
+        for (int i = 0; i < 4; i++)
+            System.out.println(bill[i]);
     }
 }
