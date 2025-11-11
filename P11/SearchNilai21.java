d63d8b103d02bb55ae2d9f40ecbf2ae5d13dde36
254107020237-crypto <254107020237@student.polinema.ac.id>
2025-11-11 23:05:07 +0700

    P11_Percobaan4_Pertanyaan2

--- P11/SearchNilai21.java
+++ P11/SearchNilai21.java
@@ -6,18 +6,27 @@ public class SearchNilai21 {
     public static void main(String[] args) {
         Scanner Akbar21 = new Scanner(System.in);
 
-        int [] arrNilai = {80, 85, 79, 96, 90, 82, 86};
-        int key = 90;
+        System.out.print("Masukkan nilai yang akan di input : ");
+        int nilai = Akbar21.nextInt();
+        int [] arrNilai = new int [nilai];
+        int key = 0;
         int hasil = 0;
 
         for (int i = 0; i < arrNilai.length; i++) {
+            System.out.print("Masukkan Nilai Mahasiswa ke - " + (i+1)+ " : ");
+            arrNilai [i] = Akbar21.nextInt();
+        }
+        System.out.print("Masukkan nilai yang di cari : ");
+        key = Akbar21.nextInt();
+
+        for (int i = 0; i < arrNilai.length; i++) {
             if (key == arrNilai[i]) {
-                hasil = i;
+                hasil = i + 1;
                 break;
             }
         }
         System.out.println();
-        System.out.println("Nilai "+key+" Ketemu di indeks ke-"+hasil);
+        System.out.println("Nilai "+key+" Ketemu nilai mahasiswa ke- "+hasil);
         System.out.println();
     }
 }
