2baaa6fb33dc694e4f78eb51a5ed9ee66e17605b
254107020237-crypto <254107020237@student.polinema.ac.id>
2025-11-11 20:50:19 +0700

    P11_Percobaan1_Pertanyaan1

--- P11/ArrayBilangan21.java
+++ P11/ArrayBilangan21.java
@@ -8,10 +8,10 @@ public class ArrayBilangan21 {
 
         int [] bil = new int [4];
 
-        bil [0] = 5;
-        bil [1] = 13;
-        bil [2] = -7;
-        bil [3] = 17;
+        bil [0] = 5.0;
+        bil [1] = 1287;
+        bil [2] = 7.5;
+        bil [3] = 2000000;
 
         System.out.println(bil [0]);
         System.out.println(bil [1]);
