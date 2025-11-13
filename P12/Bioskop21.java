03bbd82c54526425d224837d4cf09cc3afd4afaa
254107020237-crypto <254107020237@student.polinema.ac.id>
2025-11-13 16:35:16 +0700

    P12_Percobaan1

---
+++ P12/Bioskop21.java
@@ -0,0 +1,21 @@
+package P12;
+
+public class Bioskop21 {
+    public static void main(String[] args) {
+        
+        String [] [] Penonton = new String[4][2];
+
+        Penonton [0] [0] = "Amin";
+        Penonton [0] [1] = "Bena";
+        Penonton [1] [0] = "candra";
+        Penonton [1] [1] = "Dela";
+        Penonton [2] [0] = "Eka";
+        Penonton [2] [1] = "Farhan";
+        Penonton [3] [0] = "Gisel";
+
+        System.out.printf("%s \t %s \n", Penonton [0] [0], Penonton [0] [1]);
+        System.out.printf("%s \t %s \n", Penonton [1] [0], Penonton [1] [1]);
+        System.out.printf("%s \t %s \n", Penonton [2] [0], Penonton [2] [1]);
+        System.out.printf("%s \t %s \n", Penonton [3] [0], Penonton [3] [1]);
+    }
+}
