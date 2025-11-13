5df325076da5dd7a1ae766089386d7fc0a950562
254107020237-crypto <254107020237@student.polinema.ac.id>
2025-11-13 17:18:22 +0700

    P12_Percobaan3

---
+++ P12/Numbers21.java
@@ -0,0 +1,11 @@
+package P12;
+
+public class Numbers21 {
+    public static void main(String[] args) {
+
+        int [][] MyNumbers = new int [3][];
+        MyNumbers [0] = new int[5];
+        MyNumbers [1] = new int[3];
+        MyNumbers [2] = new int[1];
+    }
+}
