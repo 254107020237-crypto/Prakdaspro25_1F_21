ff96847747559559c5b2e44aead90caaf7aafa37
254107020237-crypto <254107020237@student.polinema.ac.id>
2025-12-03 14:45:05 +0700

    P14_Percobaan2_Pertanyaan6

--- P14/Kafe21.java
+++ P14/Kafe21.java
@@ -2,6 +2,20 @@ package P14;
 
 public class Kafe21 {
     public static void main(String[] args) {
+        Menu("Andi", true, "Diskon50%");
+    }
+    public static void Menu(String NamaPelanggan, Boolean isMember, String KodePromo) {
+        System.out.println("Selamat Datang, " + NamaPelanggan + "!");       
+         if (isMember) {
+            System.out.println("Anda Adalah Member, Dapatkan Diskon 10% Untuk Setiap Pembelian!");
+        }
+        if (KodePromo.equals("Diskon50%")) {
+            System.out.println("Selamat Anda Mendapatkan Diskon 50%");
+        } else if (KodePromo.equals("Diskon30%")) {
+            System.out.println("Selamat Anda Mendapatkan Diskon 30%");
+        } else {
+            System.out.println("Kode Promo Invalid");
+        }
         System.out.println("===== MENU RESTO KAFE =====");
         System.out.println("1. Kopi Hitam - Rp. 15,000");
         System.out.println("2. Cappuccino - Rp. 20,000");
@@ -11,24 +25,7 @@ public class Kafe21 {
         System.out.println("6. Mie Goreng - Rp. 18,000");
         System.out.println("===========================");
         System.out.println("Silakan Pilih Menu Yang Anda Inginkan. "); 
-        //Menu("Andi", true);
-    }
+    }    
 }
-    //public static void Menu(String NamaPelanggan, Boolean isMember) {
-        //System.out.println("Selamat Datang, " + NamaPelanggan + "!");       
-         //if (isMember) {
-            //System.out.println("Anda Adalah Member, Dapatkan Diskon 10% Untuk Setiap Pembelian!");
-        //}
-        //System.out.println("===== MENU RESTO KAFE =====");
-        //System.out.println("1. Kopi Hitam - Rp. 15,000");
-        //System.out.println("2. Cappuccino - Rp. 20,000");
-        //System.out.println("3. Latte - Rp. 22,000");
-        //System.out.println("4. Teh Tarik - Rp. 12,000");
-        //System.out.println("5. Roti Bakar - Rp. 10,000");
-        //System.out.println("6. Mie Goreng - Rp. 18,000");
-        //System.out.println("===========================");
-        //System.out.println("Silakan Pilih Menu Yang Anda Inginkan. "); 
-    //}    
-//}
    
 
