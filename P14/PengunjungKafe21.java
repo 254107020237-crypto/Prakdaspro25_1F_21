066cdb67a09c4d3ad101a8065e5b43e9b0389b22
254107020237-crypto <254107020237@student.polinema.ac.id>
2025-12-03 22:57:54 +0700

    P14_Percobaan4_Pertanyaan2

---
+++ P14/PengunjungKafe21.java
@@ -0,0 +1,16 @@
+package P14;
+
+public class PengunjungKafe21 {
+
+    public static void main(String[] args) {
+        daftarPengunjung("Ali", "Budi", "Citra");
+        daftarPengunjung("Andi");
+        daftarPengunjung("Doni","Eti","Fahmi","Galih");
+    }
+    static void daftarPengunjung(String... namaPengunjung){
+    System.out.println("Daftar Nama Pengunjung : ");
+    for (String nama : namaPengunjung) {
+    System.out.println("- " + nama);
+    }
+}
+}
